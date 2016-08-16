7c9d7f78df45f62ce6ba30bc9a170c5392b31799
Laura <lauramagalypaz@hotmail.com>
2016-08-16 14:50:09 +0200

    page view client et ajout client dans seed

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -33,44 +33,18 @@ crmControllers.controller('listClients', ['$scope', 'Rest', function($scope, Res
 					$scope.nbrPrivateprs++;
 				}
 			}
-			/*console.log(tabCompanies[0]);
-			$scope.clientsToShow.push(tabCompanies);
-			console.log($scope.clientsToShow.length);*/
-
-		$scope.showParticuliers = function(clients){
-			console.log(clients);
-			for(var i = 0; i<clients.length; i++){
-				if(clients[i].isCompany == false){
-					clientsToShow.push(clients[i]);
-				}
-			}
-			
-			$scope.clientsShow = clientsToShow;
-			clientsToShow = [];
-		};
-
-		$scope.showSociety = function(clients){
-			for(var i = 0; i<clients.length; i++){
-			if(clients[i].isCompany == true){
-					clientsToShow.push(clients[i]);
-				}
-			}
-			$scope.clientsShow = clientsToShow;
-			clientsToShow = [];
-		};
 
 		
 		});
-
 	}
 	refresh();
 
+	$scope.item = {};
 
+	$scope.showClients = function(isCmp){
+		$scope.item.isCompany = isCmp;
+	}
 
-	
-/*
-	$scope.companies = "nbrCompanies";
-	$scope.privateprs = "nbrPrivateprs";*/
 }]);
 
 crmControllers.controller('mainCtrl', ['$scope', 'Rest', function($scope, Rest){
